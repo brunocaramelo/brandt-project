0e62a587567ce4dfe39e320e99ac854af74d2303
brunocaramelo <bruno.caramelo5@gmail.com>
2021-05-15 10:09:04 -0300

    ajustes docker

--- app/portal/Home/Http/HomeController.php
+++ app/portal/Home/Http/HomeController.php
@@ -25,7 +25,7 @@ class HomeController extends Controller
 
         return view('home.index', [
                                     'banner' => $banner,
-                                    'testimonials' => $testimonials
+                                    'testimonials' => $testimonials,
                                 ]);
     }
 
