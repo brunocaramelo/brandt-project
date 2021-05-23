70adb7478a4da84397b1d249e2ea1112c962629f
Bruno Caramelo Souza <bruno.caramelo@ftd.com.br>
2021-05-23 20:13:55 -0300

    cursos ajustes

--- public/portal/html/wp-content/themes/twentynineteen/single.php
+++ public/portal/html/wp-content/themes/twentynineteen/single.php
@@ -33,16 +33,16 @@ get_header();
 					);
 				} elseif ( is_singular( 'post' ) ) {
 					// Previous/next post navigation.
-					// the_post_navigation(
-					// 	array(
-					// 		'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
-					// 			'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
-					// 			'<span class="post-title">%title</span>',
-					// 		'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
-					// 			'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
-					// 			'<span class="post-title">%title</span>',
-					// 	)
-					// );
+					the_post_navigation(
+						array(
+							'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next Post', 'twentynineteen' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Next post:', 'twentynineteen' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+							'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous Post', 'twentynineteen' ) . '</span> ' .
+								'<span class="screen-reader-text">' . __( 'Previous post:', 'twentynineteen' ) . '</span> <br/>' .
+								'<span class="post-title">%title</span>',
+						)
+					);
 				}
 
 				// If comments are open or we have at least one comment, load up the comment template.
