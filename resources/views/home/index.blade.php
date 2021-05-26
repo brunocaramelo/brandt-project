a498f3dc109fdfa2b6ba2e9b75bebb5f208e9bb1
Bruno Caramelo Souza <bruno.caramelo@ftd.com.br>
2021-05-26 01:42:11 -0300

    envio de images

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,14 +4,14 @@
 
 <header class="header valign bg-img parallaxie" style="background-position: left;" data-scroll-index="0" data-overlay-dark="0" data-background="img/bg-phot-dra.jpeg">
     <div class="container">
-        <div class="row">
+        {{-- <div class="row">
             <div class="col-md-12 text-left caption">
                 <hr class="line line-hr-primary animate-box" data-animate-effect="fadeInLeft">
                 <h5 class="animate-box" data-animate-effect="fadeInLeft">Quem é a</h5>
                 <h1 class="animate-box" data-animate-effect="fadeInLeft">Dra. Patrícia Brandt Schefer</h1>
                 <a h href={{route('about')}} class="btn fl-btn animate-box" data-animate-effect="fadeInLeft">Saiba mais</a>
             </div>
-        </div>
+        </div> --}}
         <div class="row">
             <div class="col-md-12">
                 <div class="arrow bounce text-center">
