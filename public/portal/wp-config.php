a1a96117f3c8ded5a42c44ad2832e95e6d0264e3
brunocaramelo <bruno.caramelo5@gmail.com>
2021-05-29 19:29:43 -0300

    remoceoes

--- public/portal/wp-config.php
+++ public/portal/wp-config.php
@@ -79,7 +79,7 @@ $table_prefix = 'wp_';
  *
  * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
-define( 'WP_DEBUG', true );
+define( 'WP_DEBUG', false );
 
 define('FS_METHOD','direct');
 
