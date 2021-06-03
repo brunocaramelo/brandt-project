750a324ffe48cc0990d62f3995405c84de3b4fe7
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-03 17:08:44 -0300

    melhorias

--- config/ecommerce.php
+++ config/ecommerce.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'url' => env('CMS_ORIGIN_API', ''),
+    'url' => env('WC_ORIGIN_API', ''),
     'consumer_key' => env('WC_CONSUMER_KEY', ''),
     'consumer_secret' => env('WC_CONSUMER_SECRET', ''),
 ];
