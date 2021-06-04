f69d7092891456193c59297f4715c6b5fc838fc4
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-04 18:52:55 -0300

    ajustes home

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Dra. Patrícia| Patricia Brandt')
 @section('content')
 
-<header class="header valign bg-img parallaxie" data-scroll-index="0" data-overlay-dark="0" data-background="img/banner_about.jpg" style="background-position: left;">
+<header class="header valign bg-img parallaxie" id="banner-top-rotate" data-scroll-index="0" data-overlay-dark="0" data-background="img/banner_about.jpg" style="background-position: left;">
     <div class="container" style="margin-top:420px">
         {{-- <div class="row">
             <div class="col-md-12 text-left caption">
