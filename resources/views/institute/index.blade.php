963c2f7d5f0245405c298890ab35f9befadb901a
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-04 23:07:50 -0300

    ajustes home

--- resources/views/institute/index.blade.php
+++ resources/views/institute/index.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title', 'Dra. Patrícia| Patricia Brandt')
+@section('title', 'Instituto| Patricia Brandt')
 @section('content')
 
 <header class="header valign bg-img parallaxie" data-scroll-index="0" data-overlay-dark="0" data-background="img/banner_instituto.jpg" style="background-position: left;">
@@ -20,7 +20,7 @@
     </div>
 </header>
 
-<section id="blog" class="blog section-padding" data-scroll-index="4">
+<section id="blog" class="blog section-padding" data-scroll-index="1">
     {!! $content !!}
 </section>
 
