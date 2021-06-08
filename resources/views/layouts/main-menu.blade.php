b0efcacba0e39ea4c19259f41261410f4c30847e
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-08 10:10:59 -0300

    ajuestes produtos

--- resources/views/layouts/main-menu.blade.php
+++ resources/views/layouts/main-menu.blade.php
@@ -2,7 +2,7 @@
     <ul>
         <li><a class="cool-link" href={{route('home')}}>Home</a></li>
         <li><a class="cool-link" href={{route('about')}}>Dra. Patricia</a></li>
-        <li><a class="cool-link" href="/portal/">Blog</a></li>
+        {{-- <li><a class="cool-link" href="/portal/">Blog</a></li> --}}
         <li><a class="cool-link" href={{route('general-courses')}}>Cursos</a></li>
         <li><a class="cool-link" href={{route('institute')}}>Instituto</a></li>
         <li><a class="cool-link active" href={{route('contact')}}>Contato</a></li>
