1d0072211c40671628a6d51a93edf916a518b074
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-09 13:17:54 -0300

    uploads recebeidos

--- app/portal/Home/Http/HomeController.php
+++ app/portal/Home/Http/HomeController.php
@@ -14,7 +14,7 @@ class HomeController extends Controller
     {
 
         $products = $commerceClient->get('products', [
-            'tag'=> 22,
+            'category'=> 24,
             'status'=> 'publish',
         ]);
 
