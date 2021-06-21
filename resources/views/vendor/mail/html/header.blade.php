36043c07a9159530881a8a3c4c3ae183d122bfa3
brunocaramelo <bruno.caramelo5@gmail.com>
2021-06-21 09:45:00 -0300

    logo resize

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{config('app.url')}}/img/logo-dark.jpg" style="width:200px" alt="{{ config('app.name') }}">
+<img src="{{config('app.url')}}/img/logo-dark.jpg" style="width:250px" alt="{{ config('app.name') }}">
 </a>
 </td>
 </tr>
